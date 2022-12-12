cfb0bf5efee9a7bc37d58e3802ec782cf4dd92bc
RowDog <mrdiamonddirt@gmail.com>
2022-12-12 04:08:11 +0000

    add placeholder date function

---
+++ web/src/utils/date.ts
@@ -0,0 +1,17 @@
+export const getDate = () => {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+    const second = date.getSeconds();
+    return {
+        year,
+        month,
+        day,
+        hour,
+        minute,
+        second,
+    };
+};
